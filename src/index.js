31de5aea03720d7494498cee0104cdefb4bb0e49
Никита А. Ковалевич <C:\Users\kovalevich\AppData\Roaming\The Bat!>
2020-10-14 14:18:32 +0300

    refactor: changed the index.js file to execute This task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let indexNum = 0;
+  bracketsConfig.forEach(function(item, i) {
+      bracketsConfig[i] = String(item[0] + item[1])
+    });
+    console.log(bracketsConfig);
+  while (true){
+    bracketsConfig.forEach(function(item, i) {
+      str = str.replace(item,'')
+    });
+    if (str === '') return true;
+    if (indexNum === str.length) return false;
+    indexNum = str.length;
+  }
 }
